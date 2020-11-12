3b5cbc02948ee9eabdaac00b948c67ea1fab2230
nandopeyton <68226901+nandopeyton@users.noreply.github.com>
2020-11-12 14:27:09 -0300

    removed call from category component

--- src/components/page/Category.jsx
+++ src/components/page/Category.jsx
@@ -1,21 +1,8 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import axios from "axios";
+import React from "react";
 import ProductsList from "../ProductsList";
 import CarouselComp from "../CarouselComp";
-import { getProducts } from "../../redux/actions";
 
-const Category = ({ match }) => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    axios
-      .get(`http://localhost:8000/api/v1/categories/${match.params.category}`)
-      .then((res) => {
-        dispatch(getProducts(res.data));
-      });
-  }, []);
-
+const Category = () => {
   return (
     <div className="bg-light">
       <CarouselComp />
