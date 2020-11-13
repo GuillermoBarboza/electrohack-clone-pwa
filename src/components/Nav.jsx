ecb26b0fdc8ad3e2619eaaac17917d61ee0bd203
nandopeyton <68226901+nandopeyton@users.noreply.github.com>
2020-11-13 09:15:44 -0300

    removed margin from cart

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,113 +7,112 @@ import { getUser, getProducts, changeCarousel } from "../redux/actions";
 import SearchBox from "./SearchBox";
 
 const NavBar = () => {
-	const cartQuantity = useSelector((store) =>
-		store.cart.reduce((sum, val) => sum + val.quantity, 0)
-	);
+  const cartQuantity = useSelector((store) =>
+    store.cart.reduce((sum, val) => sum + val.quantity, 0)
+  );
 
-	const user = useSelector((store) => store.user);
+  const user = useSelector((store) => store.user);
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	function handleClick(option) {
-		axios
-			.get(`http://localhost:8000/api/v1/categories/${option}`)
-			.then((res) => {
-				dispatch(getProducts(res.data[0].productsList));
-				dispatch(changeCarousel(res.data[0].banner));
-			});
-	}
+  function handleClick(option) {
+    axios
+      .get(`http://localhost:8000/api/v1/categories/${option}`)
+      .then((res) => {
+        dispatch(getProducts(res.data[0].productsList));
+        dispatch(changeCarousel(res.data[0].banner));
+      });
+  }
 
-	return (
-		<Navbar bg="light" expand="lg" fixed="top">
-			<div className="container">
-				<Navbar.Brand as={Link} to="/">
-					ELECTRO-HACK
-				</Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-					<Nav>
-						<Nav.Link
-							className="mr-4"
-							as={Link}
-							to="/"
-							onClick={() => {
-								dispatch(
-									changeCarousel([
-										"https://image.shutterstock.com/image-vector/brush-sale-banner-promotion-ribbon-260nw-1182942766.jpg",
-									])
-								);
-							}}
-						>
-							Home
-						</Nav.Link>
-						<Nav.Link
-							className="mr-4"
-							as={Link}
-							to="/categories/home-audio"
-							onClick={() => {
-								handleClick("home-audio");
-							}}
-						>
-							Home Audio
-						</Nav.Link>
-						<Nav.Link
-							className="mr-4"
-							as={Link}
-							to="/categories/bathroom"
-							onClick={() => {
-								handleClick("bathroom");
-							}}
-						>
-							Bathroom
-						</Nav.Link>
-						<Nav.Link
-							className="mr-4"
-							as={Link}
-							to="/categories/kitchen"
-							onClick={() => {
-								handleClick("kitchen");
-							}}
-						>
-							Kitchen
-						</Nav.Link>
+  return (
+    <Navbar bg="light" expand="lg" fixed="top">
+      <div className="container">
+        <Navbar.Brand as={Link} to="/">
+          ELECTRO-HACK
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
+            <Nav.Link
+              className="mr-4"
+              as={Link}
+              to="/"
+              onClick={() => {
+                dispatch(
+                  changeCarousel([
+                    "https://image.shutterstock.com/image-vector/brush-sale-banner-promotion-ribbon-260nw-1182942766.jpg",
+                  ])
+                );
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              className="mr-4"
+              as={Link}
+              to="/categories/home-audio"
+              onClick={() => {
+                handleClick("home-audio");
+              }}
+            >
+              Home Audio
+            </Nav.Link>
+            <Nav.Link
+              className="mr-4"
+              as={Link}
+              to="/categories/bathroom"
+              onClick={() => {
+                handleClick("bathroom");
+              }}
+            >
+              Bathroom
+            </Nav.Link>
+            <Nav.Link
+              className="mr-4"
+              as={Link}
+              to="/categories/kitchen"
+              onClick={() => {
+                handleClick("kitchen");
+              }}
+            >
+              Kitchen
+            </Nav.Link>
 
-						<SearchBox />
+            <SearchBox />
 
-						{user.token ? (
-							<>
-								<Nav.Link>Hello, {user.name}</Nav.Link>
-								<Nav.Link
-									className="mr-4"
-									onClick={() => {
-										dispatch(getUser({}));
-									}}
-								>
-									Logout
-								</Nav.Link>
-							</>
-						) : (
-							<Nav.Link className="mr-4" as={Link} to="/login">
-								Login
-							</Nav.Link>
-						)}
+            {user.token ? (
+              <>
+                <Nav.Link>Hello, {user.name}</Nav.Link>
+                <Nav.Link
+                  onClick={() => {
+                    dispatch(getUser({}));
+                  }}
+                >
+                  Logout
+                </Nav.Link>
+              </>
+            ) : (
+              <Nav.Link className="mr-4" as={Link} to="/login">
+                Login
+              </Nav.Link>
+            )}
 
-						<Nav.Link as={Link} to="/cart">
-							{cartQuantity > 0 ? (
-								<span className="fa-stack has-badge" data-count={cartQuantity}>
-									<i class="fas fa-lg fa-shopping-cart"></i>
-								</span>
-							) : (
-								<span className="fa-stack">
-									<i class="fas fa-lg fa-shopping-cart"></i>
-								</span>
-							)}
-						</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-			</div>
-		</Navbar>
-	);
+            <Nav.Link as={Link} to="/cart">
+              {cartQuantity > 0 ? (
+                <span className="fa-stack has-badge" data-count={cartQuantity}>
+                  <i class="fas fa-lg fa-shopping-cart"></i>
+                </span>
+              ) : (
+                <span className="fa-stack">
+                  <i class="fas fa-lg fa-shopping-cart"></i>
+                </span>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </div>
+    </Navbar>
+  );
 };
 
 export default NavBar;
