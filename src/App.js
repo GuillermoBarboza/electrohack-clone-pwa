1ee1b60a5a222208659b19520792587048abbcc9
María José Marra <majomarra@gmail.com>
2020-11-14 17:11:37 -0300

    scroll to top

--- src/App.js
+++ src/App.js
@@ -12,42 +12,44 @@ import Products from "./components/admin/page/Products";
 import Users from "./components/admin/page/Users";
 import Categories from "./components/admin/page/Categories";
 import PrivateRoute from "./components/PrivateRoute";
+import ScrollToTop from "./components/ScrollToTop";
 
 import Login from "./components/page/Login";
 import Register from "./components/page/Register";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <PublicRoute restricted={false} component={Home} path="/" exact />
-        <PublicRoute
-          restricted={false}
-          component={Category}
-          path="/categories/:category"
-          exact
-        />
-        <PublicRoute
-          restricted={false}
-          component={Product}
-          path="/products/:product"
-          exact
-        />
-        <PublicRoute restricted={false} component={Cart} path="/cart" exact />
-        <PublicRoute restricted={false} component={Login} path="/login" />
-        <PublicRoute
-          restricted={false}
-          component={Register}
-          path="/register"
-          exact
-        />
-        <PrivateRoute component={Main} path="/admin" exact />
-        <PrivateRoute component={Products} path="/admin/products" exact />
-        <PrivateRoute component={Users} path="/admin/users" exact />
-        <PrivateRoute component={Categories} path="/admin/categories" exact />
-      </Switch>
-    </Router>
-  );
+	return (
+		<Router>
+			<ScrollToTop />
+			<Switch>
+				<PublicRoute restricted={false} component={Home} path="/" exact />
+				<PublicRoute
+					restricted={false}
+					component={Category}
+					path="/categories/:category"
+					exact
+				/>
+				<PublicRoute
+					restricted={false}
+					component={Product}
+					path="/products/:product"
+					exact
+				/>
+				<PublicRoute restricted={false} component={Cart} path="/cart" exact />
+				<PublicRoute restricted={false} component={Login} path="/login" />
+				<PublicRoute
+					restricted={false}
+					component={Register}
+					path="/register"
+					exact
+				/>
+				<PrivateRoute component={Main} path="/admin" exact />
+				<PrivateRoute component={Products} path="/admin/products" exact />
+				<PrivateRoute component={Users} path="/admin/users" exact />
+				<PrivateRoute component={Categories} path="/admin/categories" exact />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
