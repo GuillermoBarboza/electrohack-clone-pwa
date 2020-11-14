f15d01463345e89e6b80c8a57e8fe6d67de97236
María José Marra <majomarra@gmail.com>
2020-11-14 14:53:27 -0300

    vista product page

--- src/components/page/Product.jsx
+++ src/components/page/Product.jsx
@@ -3,60 +3,64 @@ import { useSelector, useDispatch } from "react-redux";
 import { addToCart } from "../../redux/actions";
 
 const Product = ({ match }) => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const product = useSelector((store) => {
-    return store.products.find((product) => {
-      return (
-        product.name.toLowerCase().trim().replace(/ /g, "-") ===
-        match.params.product
-      );
-    });
-  });
+	const product = useSelector((store) => {
+		return store.products.find((product) => {
+			return (
+				product.name.toLowerCase().trim().replace(/ /g, "-") ===
+				match.params.product
+			);
+		});
+	});
 
-  return (
-    <>
-      <div className="container pt-5">
-        <div className="row pt-5">
-          <div className="col-md-8 border">
-            <img className="mt-3" src={product.image} alt="" />
-            <div className="mt-3">
-              <h3 className="font-italic border-bottom border-dark">
-                Specifications:
-              </h3>
-              <h4 className="mt-3">
-                Model:{" "}
-                <span className="font-weight-lighter">{product.name}</span>
-              </h4>
-              <h4 className="mt-3">
-                Description:{" "}
-                <p className="font-weight-lighter">{product.description}</p>
-              </h4>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="bg-dark text-light h-25">
-              <h4 className="mt-3 pt-3 text-center">{product.name}</h4>
-              <p className="ml-3">
-                {product.stock > 0 ? "available" : "not available"}
-              </p>
-            </div>
-            <div className="h-25">
-              <h4 className="mt-3 ml-3 pt-3">
-                Price: $<span className="font-italic">{product.price}</span>
-              </h4>
-              <i
-                className="btn btn-primary btn-block"
-                onClick={() => dispatch(addToCart(product))}
-              >
-                Buy
-              </i>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="container margin-product">
+				<div className="row">
+					<div className="col-lg-7">
+						<div class="card mb-4">
+							<img src={product.image} class="card-img-top" alt="Product" />
+							<div class="card-body p-5">
+								<h4 class="card-title font-italic">Specifications:</h4>
+								<hr className="my-3" />
+								<h5>Model: {product.name}</h5>
+								<p class="card-text">Description: {product.description}</p>
+								<p class="card-text">
+									<small class="text-muted">
+										<i class="far fa-comment-alt mr-1"></i> Report incorrect
+										product information.
+									</small>
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-5">
+						<div className="border">
+							<div className="product-header">
+								<h4>{product.name}</h4>
+								<p>{product.stock > 0 ? "In Stock" : "Out of Stock"}</p>
+							</div>
+							<div className="product-price">
+								<h4>
+									<span>Price: </span>${product.price}
+								</h4>
+								<p className="mb-5">
+									<small>FREE Shipping.</small>
+								</p>
+								<button
+									className="btn btn-custom btn-block"
+									onClick={() => dispatch(addToCart(product))}
+								>
+									<i class="fas fa-shopping-cart mr-2"></i> Add to Cart
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Product;
