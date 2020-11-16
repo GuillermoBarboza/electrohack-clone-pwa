5c47129d2f15c837cce97e086307ea313274c273
María José Marra <majomarra@gmail.com>
2020-11-16 15:07:19 -0300

    Create category form

--- src/components/admin/CategoryCreateForm.jsx
+++ src/components/admin/CategoryCreateForm.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-const CategoryCreateForm = () => {
-	return <div></div>;
+const CategoryCreateForm = ({ setCategories, setSearch, handleClose }) => {
+	const [name, setName] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		axios({
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			url: "http://localhost:8000/api/v1/categories",
+			data: {
+				name: name,
+			},
+		})
+			.then((res) => {
+				setCategories((categories) => [...categories, res.data]);
+				setSearch(null);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-md-12 border-bottom border-dark">
+					<h1 className="text-center">Insert new category</h1>
+				</div>
+				<div className="col-md-12 mt-3">
+					<form
+						id="form-signUp"
+						className="form-group m-auto"
+						onSubmit={(handleSubmit, handleClose)}
+					>
+						<label for="name" className="mt-1">
+							Name
+						</label>
+						<input
+							onChange={(e) => setName(e.target.value)}
+							name="name"
+							className="form-control"
+							value={name}
+							id="name"
+							type="text"
+						/>
+
+						<div className="d-flex justify-content-center mt-3">
+							<button className="btn btn-success btn-block mb-3" type="submit">
+								Add Item
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CategoryCreateForm;
