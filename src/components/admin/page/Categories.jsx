84cfc4a1f28ce2bcccbb64f12f5b2382a3b4230d
María José Marra <majomarra@gmail.com>
2020-11-17 20:48:06 -0300

    admin styles

--- src/components/admin/page/Categories.jsx
+++ src/components/admin/page/Categories.jsx
@@ -7,121 +7,121 @@ import CategoryCreateForm from "../CategoryCreateForm";
 import CategoryUpdateForm from "../CategoryUpdateForm";
 
 const Categories = () => {
-  const token = useSelector((store) => store.user.token);
-  const [categories, setCategories] = useState(null);
-  const [search, setSearch] = useState(null);
-  const [category, setCategory] = useState(null);
-  const [showCreate, setShowCreate] = useState(false);
-  const [showUpdate, setShowUpdate] = useState(false);
+	const token = useSelector((store) => store.user.token);
+	const [categories, setCategories] = useState(null);
+	const [search, setSearch] = useState(null);
+	const [category, setCategory] = useState(null);
+	const [showCreate, setShowCreate] = useState(false);
+	const [showUpdate, setShowUpdate] = useState(false);
 
-  useEffect(() => {
-    let url = "http://localhost:8000/api/v1/categories";
-    search && (url = url.concat(`/search?name=${search}`));
-    axios.get(url).then((res) => {
-      setCategories(res.data);
-    });
-  }, [search, category]);
+	useEffect(() => {
+		let url = "http://localhost:8000/api/v1/categories";
+		search && (url = url.concat(`/search?name=${search}`));
+		axios.get(url).then((res) => {
+			setCategories(res.data);
+		});
+	}, [search, category]);
 
-  const handleDelete = (_id) => {
-    axios({
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      url: "http://localhost:8000/api/v1/categories",
-      data: {
-        _id: _id,
-      },
-    }).then((res) => {
-      setCategories(categories.filter((category) => category._id !== _id));
-    });
-  };
+	const handleDelete = (_id) => {
+		axios({
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+			url: "http://localhost:8000/api/v1/categories",
+			data: {
+				_id: _id,
+			},
+		}).then((res) => {
+			setCategories(categories.filter((category) => category._id !== _id));
+		});
+	};
 
-  const closeModal = () => {
-    setShowCreate(false);
-    setShowUpdate(false);
-  };
+	const closeModal = () => {
+		setShowCreate(false);
+		setShowUpdate(false);
+	};
 
-  const showModalCreate = () => {
-    setShowCreate(true);
-  };
+	const showModalCreate = () => {
+		setShowCreate(true);
+	};
 
-  const showModalUpdate = () => {
-    setShowUpdate(true);
-  };
+	const showModalUpdate = () => {
+		setShowUpdate(true);
+	};
 
-  return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="d-flex justify-content-between">
-          {" "}
-          <SearchBox setSearch={setSearch} />
-          <button className="btn btn-success" onClick={showModalCreate}>
-            New category
-          </button>
-        </div>
+	return (
+		<div className="admin-wrapper">
+			<div className="d-flex justify-content-between">
+				<SearchBox setSearch={setSearch} />
+				<button className="btn btn-modal" onClick={showModalCreate}>
+					New category
+				</button>
+			</div>
 
-        <table className="table mt-3">
-          <thead>
-            <tr>
-              <th scope="col">Category</th>
-              <th scope="col">Category Items</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {categories &&
-              categories.map((category) => {
-                return (
-                  <tr>
-                    <td>{category.name}</td>
-                    <td>{category.productsList.length}</td>
-                    <td>
-                      <button
-                        className="btn"
-                        onClick={() => {
-                          showModalUpdate();
-                          return setCategory(category);
-                        }}
-                      >
-                        <i className="fas fa-edit"></i>
-                      </button>
-                    </td>
-                    <td>
-                      {category.productsList.length === 0 && (
-                        <button
-                          className="btn"
-                          onClick={() => handleDelete(category._id)}
-                        >
-                          <i className="fas fa-trash-alt"></i>
-                        </button>
-                      )}
-                    </td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
-        <Modal show={showUpdate} onHide={closeModal}>
-          <CategoryUpdateForm
-            category={category}
-            setCategory={setCategory}
-            setSearch={setSearch}
-            closeModal={closeModal}
-          />
-        </Modal>
+			<table className="table table-hover table-bordered mt-5">
+				<thead className="bg-table-head">
+					<tr className="text-center">
+						<th scope="col">Category</th>
+						<th scope="col">Category Items</th>
+						<th scope="col">Update</th>
+						<th scope="col">Delete</th>
+					</tr>
+				</thead>
+				<tbody>
+					{categories &&
+						categories.map((category) => {
+							return (
+								<tr>
+									<td>{category.name}</td>
+									<td className="text-center">
+										{category.productsList.length}
+									</td>
+									<td className="text-center">
+										<button
+											className="btn"
+											onClick={() => {
+												showModalUpdate();
+												return setCategory(category);
+											}}
+										>
+											<i className="fas fa-edit"></i>
+										</button>
+									</td>
+									<td className="text-center">
+										{category.productsList.length === 0 && (
+											<button
+												className="btn"
+												onClick={() => handleDelete(category._id)}
+											>
+												<i className="fas fa-trash-alt"></i>
+											</button>
+										)}
+									</td>
+								</tr>
+							);
+						})}
+				</tbody>
+			</table>
+			<Modal show={showUpdate} onHide={closeModal}>
+				<CategoryUpdateForm
+					category={category}
+					setCategory={setCategory}
+					setSearch={setSearch}
+					closeModal={closeModal}
+				/>
+			</Modal>
 
-        <Modal show={showCreate} onHide={closeModal}>
-          <CategoryCreateForm
-            setCategories={setCategories}
-            setSearch={setSearch}
-            closeModal={closeModal}
-          />
-        </Modal>
-      </div>
-    </div>
-  );
+			<Modal show={showCreate} onHide={closeModal}>
+				<CategoryCreateForm
+					setCategories={setCategories}
+					setSearch={setSearch}
+					closeModal={closeModal}
+				/>
+			</Modal>
+		</div>
+	);
 };
 
 export default Categories;
--- src/components/admin/page/Users.jsx
+++ src/components/admin/page/Users.jsx
@@ -59,75 +59,74 @@ const Users = () => {
 	};
 
 	return (
-		<div className="container mt-5">
-			<div className="row">
-				<div className="d-flex justify-content-between">
-					{" "}
-					<SearchBox setSearch={setSearch} />
-					<button className="btn btn-success" onClick={showModalCreate}>
-						New user
-					</button>
-				</div>
-
-				<table className="table mt-3">
-					<thead>
-						<tr>
-							<th scope="col">User</th>
-							<th scope="col">Email</th>
-							<th scope="col">Address</th>
-							<th scope="col">Phone</th>
-						</tr>
-					</thead>
-					<tbody>
-						{users &&
-							users.map((user) => {
-								return (
-									<tr>
-										<td>{user.name + " " + user.lastname}</td>
-										<td>{user.email}</td>
-										<td>{user.address}</td>
-										<td>{user.telephone}</td>
-										<td>
-											<button
-												className="btn"
-												onClick={() => {
-													showModalUpdate();
-													return setUser(user);
-												}}
-											>
-												<i className="fas fa-edit"></i>
-											</button>
-										</td>
-										<td>
-											<button
-												className="btn"
-												onClick={() => handleDelete(user._id)}
-											>
-												<i className="fas fa-trash-alt"></i>
-											</button>
-										</td>
-									</tr>
-								);
-							})}
-					</tbody>
-				</table>
-				<Modal size="lg" show={showUpdate} onHide={closeModal}>
-					<UserUpdateForm
-						user={user}
-						setUser={setUser}
-						setSearch={setSearch}
-						closeModal={closeModal}
-					/>
-				</Modal>
-
-				<Modal size="lg" show={showCreate} onHide={closeModal}>
-					<UserCreateForm
-						setUsers={setUsers}
-						setSearch={setSearch}
-						closeModal={closeModal}
-					/>
-				</Modal>
+		<div className="admin-wrapper">
+			<div className="d-flex justify-content-between">
+				<SearchBox setSearch={setSearch} />
+				<button className="btn btn-modal" onClick={showModalCreate}>
+					New user
+				</button>
 			</div>
+
+			<table className="table table-hover table-bordered mt-5">
+				<thead className="bg-table-head">
+					<tr className="text-center">
+						<th scope="col">User</th>
+						<th scope="col">Email</th>
+						<th scope="col">Address</th>
+						<th scope="col">Phone</th>
+						<th scope="col">Update</th>
+						<th scope="col">Delete</th>
+					</tr>
+				</thead>
+				<tbody>
+					{users &&
+						users.map((user) => {
+							return (
+								<tr>
+									<td>{user.name + " " + user.lastname}</td>
+									<td>{user.email}</td>
+									<td>{user.address}</td>
+									<td>{user.telephone}</td>
+									<td className="text-center">
+										<button
+											className="btn"
+											onClick={() => {
+												showModalUpdate();
+												return setUser(user);
+											}}
+										>
+											<i className="fas fa-edit"></i>
+										</button>
+									</td>
+									<td className="text-center">
+										<button
+											className="btn"
+											onClick={() => handleDelete(user._id)}
+										>
+											<i className="fas fa-trash-alt"></i>
+										</button>
+									</td>
+								</tr>
+							);
+						})}
+				</tbody>
+			</table>
+			<Modal size="lg" show={showUpdate} onHide={closeModal}>
+				<UserUpdateForm
+					user={user}
+					setUser={setUser}
+					setSearch={setSearch}
+					closeModal={closeModal}
+				/>
+			</Modal>
+
+			<Modal size="lg" show={showCreate} onHide={closeModal}>
+				<UserCreateForm
+					setUsers={setUsers}
+					setSearch={setSearch}
+					closeModal={closeModal}
+				/>
+			</Modal>
 		</div>
 	);
 };
