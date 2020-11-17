d25d50b703b54047f447ec837d224313d936e9ea
nandopeyton <68226901+nandopeyton@users.noreply.github.com>
2020-11-17 16:12:38 -0300

    removed search on dashboard

--- src/components/admin/page/Main.jsx
+++ src/components/admin/page/Main.jsx
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
-import Modal from "react-bootstrap/Modal";
-import SearchBox from "../../SearchBox";
 import OrderChart from "../OrderChart";
 
 const Main = () => {
   const token = useSelector((store) => store.user.token);
   const [orders, setOrders] = useState(null);
-  const [search, setSearch] = useState(null);
-  const [order, setOrder] = useState(null);
 
   useEffect(() => {
     axios({
@@ -22,18 +18,13 @@ const Main = () => {
     }).then((res) => {
       setOrders(res.data);
     });
-  }, [search, order]);
+  }, []);
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-lg-6">
           <div className="m-5">
-            <div className="d-flex justify-content-between">
-              {" "}
-              <SearchBox setSearch={setSearch} />
-            </div>
-
             <table className="table mt-3">
               <thead>
                 <tr>
