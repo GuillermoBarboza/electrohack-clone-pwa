deb2d064e825ef3720c5a5b8b5a23df114dfc5af
María José Marra <majomarra@gmail.com>
2020-11-17 12:49:53 -0300

    dashboard styles

--- src/components/admin/SideBar.jsx
+++ src/components/admin/SideBar.jsx
@@ -3,36 +3,34 @@ import { Link } from "react-router-dom";
 
 const SideBar = () => {
 	return (
-		<div>
-			<div className="sidebar d-flex flex-column justify-content-between">
-				<ul className="nav d-flex flex-column ">
-					<li className="nav-item">
-						<Link className="nav-link active" to="/admin">
-							Dashboard <span className="sr-only">(current)</span>
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" to="/admin/products">
-							Products
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" to="/admin/users">
-							Users
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" to="/admin/categories">
-							Categories
-						</Link>
-					</li>
-				</ul>
-				<Link to="/" className="text-decoration-none">
-					<h6>
-						<i className="fas fa-bolt"></i> ELECTRO-HACK
-					</h6>
-				</Link>
-			</div>
+		<div className="sidebar d-flex flex-column justify-content-between">
+			<ul className="nav d-flex flex-column ">
+				<li className="nav-item">
+					<Link className="nav-link active" to="/admin">
+						Dashboard <span className="sr-only">(current)</span>
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link className="nav-link" to="/admin/products">
+						Products
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link className="nav-link" to="/admin/users">
+						Users
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link className="nav-link" to="/admin/categories">
+						Categories
+					</Link>
+				</li>
+			</ul>
+			<Link to="/" className="text-decoration-none">
+				<h6>
+					<i className="fas fa-bolt"></i> ELECTRO-HACK
+				</h6>
+			</Link>
 		</div>
 	);
 };
--- src/components/admin/page/Main.jsx
+++ src/components/admin/page/Main.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import OrderChart from '../OrderChart'
+import OrderChart from "../OrderChart";
 
 const Main = () => {
-  return <div>
-    <h1>Le Chart</h1>
-    <OrderChart />
-  </div>;
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-lg-6"></div>
+				<div className="col-lg-6">
+					<div className="chart-wrapper">
+						<h5>Le Chart</h5>
+						<OrderChart />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Main;
