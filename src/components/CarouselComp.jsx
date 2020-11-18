c492ab412de554eafb59f9adf23d64fdbe9514d1
María José Marra <majomarra@gmail.com>
2020-11-18 17:15:25 -0300

    carousel fotos

--- src/components/CarouselComp.jsx
+++ src/components/CarouselComp.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import { useSelector } from "react-redux";
 
 const CarouselComp = () => {
-	const images = useSelector((state) => state.carousel);
-
 	return (
 		<Carousel className="carousel-margin">
 			<Carousel.Item>
 				<div
 					className="carousel-img"
 					style={{
-						backgroundImage: `url(${images[0]})`,
+						backgroundImage: `url(https://images.unsplash.com/photo-1571843439991-dd2b8e051966?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80)`,
 					}}
 					alt="First slide"
 				></div>
@@ -20,7 +17,7 @@ const CarouselComp = () => {
 				<div
 					className="carousel-img"
 					style={{
-						backgroundImage: `url(${images[1]})`,
+						backgroundImage: `url(https://images.pexels.com/photos/2747901/pexels-photo-2747901.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260)`,
 					}}
 					alt="Second slide"
 				></div>
@@ -29,9 +26,18 @@ const CarouselComp = () => {
 				<div
 					className="carousel-img"
 					style={{
-						backgroundImage: `url(${images[2]})`,
+						backgroundImage: `url(https://images.pexels.com/photos/3623785/pexels-photo-3623785.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260)`,
 					}}
 					alt="Third slide"
+				></div>
+			</Carousel.Item>
+			<Carousel.Item>
+				<div
+					className="carousel-img"
+					style={{
+						backgroundImage: `url(https://images.pexels.com/photos/1457842/pexels-photo-1457842.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260)`,
+					}}
+					alt="Fourth slide"
 				></div>
 			</Carousel.Item>
 		</Carousel>
