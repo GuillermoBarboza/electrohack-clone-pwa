6c5e8a06376aa4c7167485832b644d8e798a73db
nandopeyton <68226901+nandopeyton@users.noreply.github.com>
2020-11-19 17:22:00 -0300

    added featured route

--- src/components/page/Home.jsx
+++ src/components/page/Home.jsx
@@ -7,22 +7,22 @@ import CarouselComp from "../CarouselComp";
 import { getProducts } from "../../redux/actions";
 
 const Home = () => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		axios.get(`${globalUrl}/api/v1/products`).then((res) => {
-			dispatch(getProducts(res.data));
-		});
-	}, []);
+  useEffect(() => {
+    axios.get(`${globalUrl}/api/v1/products/featured`).then((res) => {
+      dispatch(getProducts(res.data));
+    });
+  }, []);
 
-	return (
-		<div>
-			<CarouselComp />
-			<div className="container">
-				<ProductsList />
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <CarouselComp />
+      <div className="container">
+        <ProductsList />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
