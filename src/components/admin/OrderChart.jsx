7faf012ffb6475821ab83e927f2cfd11d9fe0644
nandopeyton <68226901+nandopeyton@users.noreply.github.com>
2020-11-19 16:20:19 -0300

    fixed orderchart

--- src/components/admin/OrderChart.jsx
+++ src/components/admin/OrderChart.jsx
@@ -1,53 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
 import Chart from "chart.js";
-import axios from "axios";
-import globalUrl from "../../utils/url";
 
-const OrderChart = () => {
-  const token = useSelector((store) => store.user.token);
-  const [orders, setOrders] = useState([]);
-  const [timelapse, setTimelapse] = useState([]);
-
+const OrderChart = ({ orders }) => {
   useEffect(() => {
-    axios({
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      url: `${globalUrl}/api/v1/orders/chart`,
-    })
-      .then((res) => {
-        console.log(res.data)
-        setOrders(res.data.map(order => order.total));
-        setTimelapse([...Array(30)].map((_, idx) => idx + 1));
-      })
-      .catch((err) => {
-        console.log(err);
+    if (orders) {
+      const ctx = document.getElementById("myChart");
+      const chart = new Chart(ctx, {
+        type: "line",
+
+        data: {
+          labels: [...Array(30)].map((_, idx) => idx + 1),
+          datasets: [
+            {
+              label: "Order sales report.",
+              backgroundColor: "rgb(255, 99, 132)",
+              borderColor: "rgb(255, 99, 132)",
+              data: orders.map((order) => order.total),
+            },
+          ],
+        },
+
+        options: {},
       });
-  }, []);
-
-  useEffect(() => {
-    const ctx = document.getElementById("myChart");
-    const chart = new Chart(ctx, {
-      type: "line",
-
-      data: {
-        labels: timelapse,
-        datasets: [
-          {
-            label: "Order sales report.",
-            backgroundColor: "rgb(255, 99, 132)",
-            borderColor: "rgb(255, 99, 132)",
-            data: orders,
-          },
-        ],
-      },
-
-      options: {},
-    });
-  }, [orders, timelapse]);
+    }
+  }, [orders]);
 
   return <canvas id="myChart"></canvas>;
 };
--- src/components/admin/page/Main.jsx
+++ src/components/admin/page/Main.jsx
@@ -5,57 +5,57 @@ import globalUrl from "../../../utils/url";
 import OrderChart from "../OrderChart";
 
 const Main = () => {
-	const token = useSelector((store) => store.user.token);
-	const [orders, setOrders] = useState(null);
+  const token = useSelector((store) => store.user.token);
+  const [orders, setOrders] = useState(null);
 
-	useEffect(() => {
-		axios({
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${token}`,
-			},
-			url: `${globalUrl}/api/v1/orders`,
-		}).then((res) => {
-			setOrders(res.data);
-		});
-	}, []);
+  useEffect(() => {
+    axios({
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      url: `${globalUrl}/api/v1/orders`,
+    }).then((res) => {
+      setOrders(res.data);
+    });
+  }, []);
 
-	return (
-		<div className="container-fluid">
-			<div className="admin-wrapper">
-				<div className="chart-wrapper bg-light shadow">
-					<h5>Monthly Sales</h5>
-					<OrderChart />
-				</div>
-				<div className="table-responsive">
-					<table className="table table-hover table-bordered mt-4">
-						<thead className="bg-table-head">
-							<tr className="text-center">
-								<th scope="col">Buyer name</th>
-								<th scope="col">Items Qty</th>
-								<th scope="col">State</th>
-								<th scope="col">Total</th>
-							</tr>
-						</thead>
-						<tbody>
-							{orders &&
-								orders.map((order) => {
-									return (
-										<tr>
-											<td>{order.buyer.name}</td>
-											<td className="text-center">{order.products.length}</td>
-											<td className="text-center">{order.state}</td>
-											<td className="text-center">${order.total}</td>
-										</tr>
-									);
-								})}
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="container-fluid">
+      <div className="admin-wrapper">
+        <div className="chart-wrapper bg-light shadow">
+          <h5>Monthly Sales</h5>
+          <OrderChart orders={orders} />
+        </div>
+        <div className="table-responsive">
+          <table className="table table-hover table-bordered mt-4">
+            <thead className="bg-table-head">
+              <tr className="text-center">
+                <th scope="col">Buyer name</th>
+                <th scope="col">Items Qty</th>
+                <th scope="col">State</th>
+                <th scope="col">Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders &&
+                orders.map((order) => {
+                  return (
+                    <tr>
+                      <td>{order.buyer.name}</td>
+                      <td className="text-center">{order.products.length}</td>
+                      <td className="text-center">{order.state}</td>
+                      <td className="text-center">${order.total}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
