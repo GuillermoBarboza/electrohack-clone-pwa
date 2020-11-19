22a82e5659d767c3e5d11fe7e8955e3f6cf81687
María José Marra <majomarra@gmail.com>
2020-11-19 17:37:19 -0300

    styles scroll to top

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,18 +1,19 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import ProductCard from "./ProductCard";
 const ProductsList = () => {
 	const productStore = useSelector((store) => store.products);
-	const [scroll, setScroll] = useState(true)
-
+	const [scroll, setScroll] = useState(true);
 
 	useEffect(() => {
 		window.scrollTo(0, 0);
-	  }, [scroll]);
+	}, [scroll]);
 
 	return (
 		<div className="mx-auto pb-5">
-		<button id='myBtn' onClick={()=> setScroll(!scroll)}>Go to top</button>
+			<button id="myBtn" onClick={() => setScroll(!scroll)}>
+				<i class="fas fa-chevron-up"></i>
+			</button>
 			<div className="row">
 				{productStore.length > 0 &&
 					productStore.map((product) => <ProductCard product={product} />)}
