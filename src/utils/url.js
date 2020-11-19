9c434f1ee84c3d7ee42ac8979d7b6b6c1e70a46e
nandopeyton <68226901+nandopeyton@users.noreply.github.com>
2020-11-19 14:55:21 -0300

    vercel

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,6 +1,6 @@
 const vercelUrl = "https://back-end-swart.vercel.app";
 const localUrl = "http://localhost:8000";
 
-const globalUrl = localUrl;
+const globalUrl = vercelUrl;
 
 export default globalUrl;
