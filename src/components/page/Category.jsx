0078f826b3cbc0b02c10fc838c6fe1bab465ab31
María José Marra <majomarra@gmail.com>
2020-11-20 07:25:13 -0300

    styles

--- src/components/page/Category.jsx
+++ src/components/page/Category.jsx
@@ -3,14 +3,18 @@ import ProductsList from "../ProductsList";
 import CarouselComp from "../CarouselComp";
 
 const Category = () => {
-  return (
-    <div>
-      <CarouselComp />
-      <div className="container">
-        <ProductsList />
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="overlay">
+				<h1 className="carousel-heading">Home Appliances</h1>
+				<h4 className="carousel-subheading">Your perfect house is here</h4>
+			</div>
+			<CarouselComp />
+			<div className="container pt-5">
+				<ProductsList />
+			</div>
+		</div>
+	);
 };
 
 export default Category;
--- src/components/page/Home.jsx
+++ src/components/page/Home.jsx
@@ -7,26 +7,30 @@ import CarouselComp from "../CarouselComp";
 import { getProducts } from "../../redux/actions";
 
 const Home = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    axios.get(`${globalUrl}/api/v1/products/featured`).then((res) => {
-      dispatch(getProducts(res.data));
-    });
-  }, []);
+	useEffect(() => {
+		axios.get(`${globalUrl}/api/v1/products/featured`).then((res) => {
+			dispatch(getProducts(res.data));
+		});
+	}, []);
 
-  return (
-    <div>
-      <CarouselComp />
-      <div className="container">
-        <div className="text-center">
-          <h2 className="mb-4 ">Featured Products</h2>
-        </div>
+	return (
+		<div>
+			<div className="overlay">
+				<h1 className="carousel-heading">Home Appliances</h1>
+				<h4 className="carousel-subheading">Your perfect house is here</h4>
+			</div>
+			<CarouselComp />
+			<div className="container">
+				<div className="text-center">
+					<h2 className="mb-4">Featured Products</h2>
+				</div>
 
-        <ProductsList />
-      </div>
-    </div>
-  );
+				<ProductsList />
+			</div>
+		</div>
+	);
 };
 
 export default Home;
