3f3cdca8db491a007a1e9b7de000505ea5d4cb19
María José Marra <majomarra@gmail.com>
2020-11-20 16:14:15 -0300

    responsive admin

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,15 +3,48 @@ import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { isAdmin } from "../utils";
 import SideBar from "./admin/SideBar";
+import { Navbar, Nav, ListGroup, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
 	const user = useSelector((state) => state.user);
 	return (
 		<div className="row bg-admin-color">
-			<div className="col-md-2">
+			<div className="d-none d-md-block col-md-2">
 				<SideBar />
 			</div>
-			<div className="col-md-10">
+			<div className="d-block d-md-none">
+				<Navbar className="bg-nav-admin" variant="dark" expand="lg" fixed="top">
+					<div className="container">
+						<Navbar.Brand as={Link} to="/" className="brand-font">
+							<i className="fas fa-xs fa-bolt"></i> Electro<strong>Hack</strong>
+						</Navbar.Brand>
+						<Navbar.Toggle aria-controls="basic-navbar-nav" />
+						<Navbar.Collapse
+							id="basic-navbar-nav"
+							className="justify-content-end"
+						>
+							<Nav>
+								<Nav.Link as={Link} to="/admin" className="mr-4">
+									Dashboard
+								</Nav.Link>
+
+								<Nav.Link as={Link} to="/admin/products" className="mr-4">
+									Products
+								</Nav.Link>
+								<Nav.Link as={Link} to="/admin/users" className="mr-4">
+									Users
+								</Nav.Link>
+
+								<Nav.Link as={Link} to="/admin/categories">
+									Categories
+								</Nav.Link>
+							</Nav>
+						</Navbar.Collapse>
+					</div>
+				</Navbar>
+			</div>
+			<div className="col-12 col-md-10">
 				<Route
 					{...rest}
 					render={(props) =>
