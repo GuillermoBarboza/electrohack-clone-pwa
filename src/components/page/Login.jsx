30659b5f5489ea5e3fa9aa470203b9bcde7a09a4
nandopeyton <68226901+nandopeyton@users.noreply.github.com>
2020-11-20 12:06:26 -0300

    added front validation

--- src/components/page/Login.jsx
+++ src/components/page/Login.jsx
@@ -8,27 +8,69 @@ import globalUrl from "../../utils/url";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailErr, setEmailErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
+
+  const formValidation = () => {
+    const passwordErr = {};
+    const emailErr = {};
+    let isValid = true;
+
+    if (password === "") {
+      passwordErr.passwordEmpty = "this is a required field";
+      isValid = false;
+    }
+
+    if (password.trim().length < 4) {
+      passwordErr.passwordShort = "password should have at least 4 characters";
+      isValid = false;
+    }
+
+    if (password.trim().length > 10) {
+      passwordErr.passwordLong = "password should have less than 10 characters";
+      isValid = false;
+    }
+
+    if (email === "") {
+      emailErr.emailEmpty = "this is a required field";
+      isValid = false;
+    }
+
+    const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (!email.match(mailformat)) {
+      emailErr.passwordFormat = "email should be a valid email address";
+      isValid = false;
+    }
+
+    setPasswordErr(passwordErr);
+    setEmailErr(emailErr);
+    return isValid;
+  };
+
   const dispatch = useDispatch();
   const history = useHistory();
 
   const handleLogin = (e) => {
     e.preventDefault();
-    axios({
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      url: `${globalUrl}/api/v1/users/find`,
-      data: {
-        email: email,
-        password: password,
-      },
-    })
-      .then((res) => {
-        dispatch(getUser(res.data));
-        history.goBack();
+    const isValid = formValidation();
+    isValid &&
+      axios({
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        url: `${globalUrl}/api/v1/users/find`,
+        data: {
+          email: email,
+          password: password,
+        },
       })
-      .catch((err) => {
-        console.log(err);
-      });
+        .then((res) => {
+          dispatch(getUser(res.data));
+          history.goBack();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
   };
 
   return (
@@ -51,7 +93,9 @@ const Login = () => {
             id="email"
             type="text"
           />
-
+          {Object.keys(emailErr).map((key) => {
+            return <div className="alert-danger">{emailErr[key]}</div>;
+          })}
           <label htmlFor="password">Password</label>
           <input
             onChange={(e) => setPassword(e.target.value)}
@@ -60,6 +104,9 @@ const Login = () => {
             id="password"
             type="password"
           />
+          {Object.keys(passwordErr).map((key) => {
+            return <div className="alert-danger">{passwordErr[key]}</div>;
+          })}
           <button
             className="btn btn-logIn btn-block custom-shadow"
             type="submit"
--- src/components/page/Register.jsx
+++ src/components/page/Register.jsx
@@ -12,32 +12,135 @@ const Register = () => {
   const [address, setAddress] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [nameErr, setNameErr] = useState("");
+  const [lastnameErr, setLastnameErr] = useState("");
+  const [telephoneErr, setTelephoneErr] = useState("");
+  const [addressErr, setAddressErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
+  const [emailErr, setEmailErr] = useState("");
+
+  const formValidation = () => {
+    const nameErr = {};
+    const lastnameErr = {};
+    const telephoneErr = {};
+    const addressErr = {};
+    const passwordErr = {};
+    const emailErr = {};
+    let isValid = true;
+
+    if (name === "") {
+      nameErr.nameEmpty = "this is a required field";
+      isValid = false;
+    }
+
+    if (name.trim().length < 3) {
+      nameErr.nameShort = "name should have at least 3 characters";
+      isValid = false;
+    }
+
+    if (name.trim().length > 10) {
+      nameErr.nameLong = "name should have less than 10 characters";
+      isValid = false;
+    }
+
+    if (lastname === "") {
+      lastnameErr.nameEmpty = "this is a required field";
+      isValid = false;
+    }
+
+    if (lastname.trim().length < 3) {
+      lastnameErr.lastnameShort = "name should have at least 3 characters";
+      isValid = false;
+    }
+
+    if (lastname.trim().length > 10) {
+      lastnameErr.lastnameLong = "name should have less than 10 characters";
+      isValid = false;
+    }
+
+    if (telephone === "") {
+      telephoneErr.telephoneEmpty = "this is a required field";
+      isValid = false;
+    }
+
+    if (isNaN(telephone)) {
+      telephoneErr.notNumber = "this field should be a number";
+      isValid = false;
+    }
+
+    if (telephone.trim().length > 15) {
+      lastnameErr.lastnameLong = "phone should have less than 15 characters";
+      isValid = false;
+    }
+
+    if (address === "") {
+      addressErr.addressEmpty = "this is a required field";
+      isValid = false;
+    }
+
+    if (password === "") {
+      passwordErr.passwordEmpty = "this is a required field";
+      isValid = false;
+    }
+
+    if (password.trim().length < 4) {
+      passwordErr.passwordShort = "password should have at least 4 characters";
+      isValid = false;
+    }
+
+    if (password.trim().length > 10) {
+      passwordErr.passwordLong = "password should have less than 10 characters";
+      isValid = false;
+    }
+
+    if (email === "") {
+      emailErr.emailEmpty = "this is a required field";
+      isValid = false;
+    }
+
+    const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (!email.match(mailformat)) {
+      emailErr.passwordFormat = "email should be a valid email address";
+      isValid = false;
+    }
+
+    setNameErr(nameErr);
+    setLastnameErr(lastnameErr);
+    setTelephoneErr(telephoneErr);
+    setAddressErr(addressErr);
+    setPasswordErr(passwordErr);
+    setEmailErr(emailErr);
+    return isValid;
+  };
 
   const dispatch = useDispatch();
   const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios({
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      url: `${globalUrl}/api/v1/users/create`,
-      data: {
-        name: name,
-        lastname: lastname,
-        telephone: telephone,
-        password: password,
-        email: email,
-        address: address,
-      },
-    })
-      .then((res) => {
-        dispatch(getUser(res.data));
-        history.goBack();
+    const isValid = formValidation();
+    isValid &&
+      axios({
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        url: `${globalUrl}/api/v1/users/create`,
+        data: {
+          name: name,
+          lastname: lastname,
+          telephone: telephone,
+          password: password,
+          email: email,
+          address: address,
+        },
       })
-      .catch((err) => {
-        console.log(err);
-      });
+        .then((res) => {
+          dispatch(getUser(res.data));
+          history.goBack();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
   };
 
   return (
@@ -57,7 +160,9 @@ const Register = () => {
             id="firstname"
             type="text"
           />
-
+          {Object.keys(nameErr).map((key) => {
+            return <div className="alert-danger">{nameErr[key]}</div>;
+          })}
           <label htmlFor="lastname">Lastname</label>
           <input
             onChange={(e) => setLastname(e.target.value)}
@@ -66,7 +171,9 @@ const Register = () => {
             id="lastname"
             type="text"
           />
-
+          {Object.keys(lastnameErr).map((key) => {
+            return <div className="alert-danger">{lastnameErr[key]}</div>;
+          })}
           <label htmlFor="address">Address</label>
           <input
             onChange={(e) => setAddress(e.target.value)}
@@ -75,7 +182,9 @@ const Register = () => {
             id="username"
             type="text"
           />
-
+          {Object.keys(addressErr).map((key) => {
+            return <div className="alert-danger">{addressErr[key]}</div>;
+          })}
           <label htmlFor="telephone">Telephone</label>
           <input
             onChange={(e) => setTelephone(e.target.value)}
@@ -84,7 +193,9 @@ const Register = () => {
             id="telephone"
             type="text"
           />
-
+          {Object.keys(telephoneErr).map((key) => {
+            return <div className="alert-danger">{telephoneErr[key]}</div>;
+          })}
           <label htmlFor="email">Email</label>
           <input
             onChange={(e) => setEmail(e.target.value)}
@@ -93,7 +204,9 @@ const Register = () => {
             id="email"
             type="email"
           />
-
+          {Object.keys(emailErr).map((key) => {
+            return <div className="alert-danger">{emailErr[key]}</div>;
+          })}
           <label htmlFor="password">Password</label>
           <input
             onChange={(e) => setPassword(e.target.value)}
@@ -102,6 +215,9 @@ const Register = () => {
             id="password"
             type="password"
           />
+          {Object.keys(passwordErr).map((key) => {
+            return <div className="alert-danger">{passwordErr[key]}</div>;
+          })}
           <div className="d-flex justify-content-center mt-2">
             <button
               className="btn btn-logIn custom-shadow btn-block"
