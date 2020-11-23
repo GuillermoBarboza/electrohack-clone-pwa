0a221499280f591e04444a0fe83dc6f44a980d34
nandopeyton <68226901+nandopeyton@users.noreply.github.com>
2020-11-23 11:55:22 -0300

    added modal to home

---
+++ src/components/HomeModal.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { getUser } from "../redux/actions";
+import globalUrl from "../utils/url";
+
+const HomeModal = ({ setShowModal }) => {
+  const dispatch = useDispatch();
+  const [database, setDatabase] = useState("dbOnline");
+  const [databaseClass, setDatabaseClass] = useState("");
+  const [serverResponse, setServerResponse] = useState();
+
+  useEffect(() => {
+    if (database === "dbOnline") {
+      window.scrollTo(0, 0);
+    }
+    switch (database) {
+      case "dbOnline":
+        return setDatabaseClass("alert-danger");
+      case "dbRefreshing":
+        return setDatabaseClass("alert-primary");
+      case "dbRestored":
+        return setDatabaseClass("alert-success");
+      default:
+        return setDatabaseClass("");
+    }
+  }, [database]);
+
+  function refreshDB() {
+    setDatabase("dbRefreshing");
+    dispatch(getUser({}));
+    axios({
+      method: "GET",
+      url: `${globalUrl}/api/v1/seed`,
+    })
+      .then((response) => {
+        setDatabase("dbRestored");
+        setServerResponse(response.data);
+        setShowModal(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <div className="modal-color rounded">
+      <div className="row mx-4">
+        <div className="col-md-6">
+          <div className="p-md-5">
+            <h5 className="mb-3">Use these credentials to try our page!</h5>
+            <div className="ml-3">
+              <p className="m-0">As a Customer</p>
+              <p className="my-0 ml-3">Email: user@user.com</p>
+              <p className="my-0 ml-3">Password: user</p>
+              <p className="mt-2 mb-0">As an Admin, with full CRUD access!</p>
+              <p className="m-0 ml-3">Email: admin@admin.com</p>
+              <p className="m-0 ml-3">Password: admin</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="p-md-5">
+            <h5 className="mb-3">Press this button to begin!</h5>
+            <div className="text-center mt-4">
+              <button
+                onClick={refreshDB}
+                className={`btn btn:block shadow-sm py-3 px-5 ${databaseClass}`}
+              >
+                {database === "dbOnline" && "Reset Database"}
+                {database === "dbRefreshing" && "Resetting Database..."}
+                {database === "dbRestored" && serverResponse}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeModal;
--- src/components/page/Home.jsx
+++ src/components/page/Home.jsx
@@ -1,38 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import Modal from "react-bootstrap/Modal";
 import axios from "axios";
 import globalUrl from "../../utils/url";
 import ProductsList from "../ProductsList";
 import CarouselComp from "../CarouselComp";
+import HomeModal from "../HomeModal";
 import { getProducts } from "../../redux/actions";
 
 const Home = () => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(true);
 
-	useEffect(() => {
-		axios.get(`${globalUrl}/api/v1/products/featured`).then((res) => {
-			dispatch(getProducts(res.data));
-		});
-	}, []);
+  useEffect(() => {
+    axios.get(`${globalUrl}/api/v1/products/featured`).then((res) => {
+      dispatch(getProducts(res.data));
+    });
+  }, []);
 
-	return (
-		<div>
-			<div className="d-none d-md-block  overlay">
-				<h1 className="d-none d-md-block carousel-heading">Home Appliances</h1>
-				<h4 className="d-none d-md-block carousel-subheading">
-					Your perfect house is here
-				</h4>
-			</div>
-			<CarouselComp />
-			<div className="container">
-				<div className="text-center">
-					<h2 className="mb-4">Featured Products</h2>
-				</div>
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
-				<ProductsList />
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="d-none d-md-block  overlay">
+        <h1 className="d-none d-md-block carousel-heading">Home Appliances</h1>
+        <h4 className="d-none d-md-block carousel-subheading">
+          Your perfect house is here
+        </h4>
+      </div>
+      <CarouselComp />
+      <div className="container">
+        <div className="text-center">
+          <h2 className="mb-4">Featured Products</h2>
+        </div>
+
+        <ProductsList />
+      </div>
+      <Modal
+        size="lg"
+        backdrop="static"
+        keyboard="false"
+        show={showModal}
+        onHide={closeModal}
+      >
+        <HomeModal setShowModal={setShowModal} />
+      </Modal>
+    </div>
+  );
 };
 
 export default Home;
