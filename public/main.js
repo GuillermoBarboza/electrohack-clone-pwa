c42a679ae27e42a68915d80f2d19d97b5e148ae6
Guishe <gui.bar.jim@gmail.com>
2020-12-23 14:54:33 -0300

    added service worker, index.html now calls main.js and styles.css

---
+++ public/main.js
@@ -0,0 +1,46 @@
+window.onload = () => {
+    "use strict";
+    console.log("inicios");
+  
+    if ("serviceWorker" in navigator) {
+      console.log("navigator", navigator);
+      console.log(navigator.serviceWorker);
+      navigator.serviceWorker
+        .register("./serviceWorker.js")
+        .then((res) => {
+          console.log(res.scope);
+        })
+        .catch((error) => {
+          // registration failed
+          console.log("Registration failed with " + error);
+        });
+    }
+  };
+  
+  /* let deferredPrompt;
+  const installBtn = document.querySelector(".install");
+  installBtn.style.display = "none";
+  
+  window.addEventListener("beforeinstallprompt", (e) => {
+    console.log(e);
+  
+    e.preventDefault();
+  
+    deferredPrompt = e;
+  
+    installBtn.style.display = "block";
+  
+    installBtn.addEventListener("click", (e) => {
+      installBtn.style.display = "none";
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("User accepted the A2HS prompt");
+        } else {
+          console.log("User dismissed the A2HS prompt");
+        }
+        deferredPrompt = null;
+      });
+    });
+  }); */
+  
---
+++ public/serviceWorker.js
@@ -0,0 +1,46 @@
+var cacheName = 'electrohackPWA1.0.0';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/main.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log(cache, 'on install')
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  console.log('on fetch')
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      console.log(response)
+      
+      return response || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  var cacheKeeplist = cacheName;
+
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        console.log(cacheKeeplist.indexOf(key));
+        console.log(key);
+        
+        if (cacheKeeplist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
